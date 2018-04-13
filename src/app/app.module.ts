350734ea28aa8efde93dac9e54dd2ae4396753b8
SaraDavidovic1924 <saradavidovic1924@gmail.com>
2018-04-13 15:28:47 +0200

    slideShow popravljen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule
